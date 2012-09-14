4697ca82d7c1a87ca6b905641c974775a17662c7
Vinícius <computationalcore@gmail.com>
2012-09-14 20:39:55 -0300

    Added classes to convert Datastore models to Json. /ranking now return a list of first 10 best scores

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import webapp2
 from google.appengine.ext import db
 
+import utils
+
+def player_key(player_email=None):
+    """Constructs a Datastore key for a {layer entity with player_email."""
+
+    return db.Key.from_path('Player', player_email)
+
+
 
 class Player(db.Model):
-    """Models an individual Guestbook entry with an author, content, and date."""
+    """Models an individual Player entry with an author"""
     nickname = db.StringProperty()
-    email = db.StringProperty(required=True,name="key")
+    email = db.StringProperty(required=True)
     score = db.IntegerProperty()
     turns = db.IntegerProperty()
     lat = db.FloatProperty()
@@ -16,6 +24,10 @@ class Player(db.Model):
     provider = db.StringProperty()
     accuracy = db.FloatProperty()
     date = db.DateTimeProperty(auto_now_add=True)
+
+    def to_dict(self):
+        return dict((p, unicode(getattr(self, p))) for p in self.properties()
+            if getattr(self, p) is not None)
 
 
 class MainPage(webapp2.RequestHandler):
@@ -31,49 +43,64 @@ class Response(webapp2.RequestHandler):
                                 "FROM Player "
                                 "ORDER BY score DESC LIMIT 10")
 
-        for player in players:
-            self.response.out.write(
-                '<b>%s</b> - Score: %s' % (player.nickname, player.score) )
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(utils.encode(players))
+
 
 class Publisher(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.out.write('1')
+
     def post(self):
         """
         Receive the data and store the entry into the datastore.
+        It returns a code error or the id of the user
+        return a code error.
+        Code Error
+        -1 - Invalid Data (It have some wrong type data)
+        0 - Missing field (Pass on the validation, but something is missing)
+
         """
-        nickname = self.request.get('nickname')
-        email = self.request.get('email')
-        score = self.request.get('score')
-        turn = self.request.get('turn')
-        lat = self.request.get('lat')
-        lon = self.request.get('lon')
-        city = self.request.get('city')
-        state = self.request.get('state')
-        country = self.request.get('country')
-        provider = self.request.get('provider')
-        accuracy = self.request.get('accuracy')
 
-        if (nickname and email and score and turn and lat and lon and provider
-            and accuracy):
-            player = Model.get(email)
-            if (not player):
-                player = Player(email=email)
-            player.nickname = nickname
-            player.score = score
-            player.turns = turn
-            player.lat = lat
-            player.lon = lon
-            player.city = city
-            player.state = state
-            player.country = country
-            player.provider = provider
-            player.accuracy = accuracy
-            player.put()
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        try:
+            nickname = self.request.get('nickname')
+            email = self.request.get('email')
+            score = int(self.request.get('score'))
+            turn = int(self.request.get('turns'))
+            lat = float(self.request.get('lat'))
+            lon = float(self.request.get('lon'))
+            city = self.request.get('city')
+            state = self.request.get('state')
+            country = self.request.get('country')
+            provider = self.request.get('provider')
+            accuracy = float(self.request.get('accuracy'))
+        except:
             self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('1')
-        else:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('0')
+            self.response.write('-1')
+            return None
+
+        #if nickname and email and score and turn and lat and lon and provider and accuracy
+
+        player =  Player(email=email)
+        player.nickname = nickname
+        player.score = score
+        player.turns = turn
+        player.lat = lat
+        player.lon = lon
+        player.city = city
+        player.state = state
+        player.country = country
+        player.provider = provider
+        player.accuracy = accuracy
+        player.put()
+        self.response.write(player.key())
+        #else:
+        #    self.response.headers['Content-Type'] = 'text/plain'
+        #    self.response.write('0')
 
 
-app = webapp2.WSGIApplication([('/', MainPage), ('/publish', Publisher), ('/georanking', Response)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage), ('/publish', Publisher), ('/ranking', Response)], debug=True)
 
---
+++ utils.py
@@ -0,0 +1,67 @@
+"""Utility classes and methods for use with json and appengine.
+
+Provides both a specialized json encoder, GqlEncoder, designed to simplify
+encoding directly from GQL results to JSON. A helper function, encode, is also
+provided to further simplify usage.
+
+  GqlEncoder: Adds support for GQL results and properties to json.
+  encode(input): Direct method to encode GQL objects as JSON.
+"""
+
+import datetime
+import json
+import time
+
+from google.appengine.api import users
+from google.appengine.ext import db
+
+
+class GqlEncoder(json.JSONEncoder):
+
+    """Extends JSONEncoder to add support for GQL results and properties.
+
+    Adds support to json JSONEncoders for GQL results and properties by
+    overriding JSONEncoder's default method.
+    """
+
+    # TODO Improve coverage for all of App Engine's Property types.
+
+    def default(self, obj):
+
+        """Tests the input object, obj, to encode as JSON."""
+
+        if hasattr(obj, '__json__'):
+            return getattr(obj, '__json__')()
+
+        if isinstance(obj, db.GqlQuery):
+            return list(obj)
+
+        elif isinstance(obj, db.Model):
+            properties = obj.properties().items()
+            output = {}
+            for field, value in properties:
+                #Convert Datastore time and convert to timestamp
+                obj_field = getattr(obj, field)
+                if isinstance(obj_field, datetime.datetime):
+                    output[field] = time.mktime(obj_field.timetuple())
+                else:
+                    output[field] = getattr(obj, field)
+            return output
+
+        return json.JSONEncoder.default(self, obj)
+
+
+def encode(input):
+    """Encode an input GQL object as JSON
+
+    Args:
+      input: A GQL object or DB property.
+
+    Returns:
+      A JSON string based on the input object.
+
+    Raises:
+      TypeError: Typically occurs when an input object contains an unsupported
+        type.
+    """
+    return GqlEncoder().encode(input)
