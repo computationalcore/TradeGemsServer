2d8eb62249701a14a716f3d65d4ed2a5737efd9c
Vinícius <computationalcore@gmail.com>
2012-09-15 03:22:46 -0300

    Added ancestor query and Highly Consistent Data for a single user to query for his own results. Also some validation added

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import webapp2
 from google.appengine.ext import db
+import hashlib
 
 import utils
 
-def player_key(player_email=None):
-    """Constructs a Datastore key for a {layer entity with player_email."""
+def user_key(user_email=None):
+    """Constructs a Datastore key for a User entity with player_email."""
 
-    return db.Key.from_path('Player', player_email)
-
-
+    return db.Key.from_path('User', user_email)
 
 class Player(db.Model):
     """Models an individual Player entry with an author"""
     nickname = db.StringProperty()
-    email = db.StringProperty(required=True)
+    email = db.StringProperty()
     score = db.IntegerProperty()
     turns = db.IntegerProperty()
     lat = db.FloatProperty()
@@ -36,12 +35,21 @@ class MainPage(webapp2.RequestHandler):
         self.response.write('Trade Gems!')
 
 
-
 class Response(webapp2.RequestHandler):
     def get(self):
-        players = db.GqlQuery("SELECT * "
-                                "FROM Player "
-                                "ORDER BY score DESC LIMIT 10")
+
+        parent_key = self.request.get('p')
+
+        if parent_key:
+            players = db.GqlQuery("SELECT * "
+                              "FROM Player "
+                              "WHERE ANCESTOR IS :1 "
+                              "ORDER BY score DESC LIMIT 10",
+                              user_key(parent_key))
+        else:
+            players = db.GqlQuery("SELECT * "
+            "FROM Player "
+            "ORDER BY score DESC LIMIT 10")
 
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(utils.encode(players))
@@ -66,16 +74,16 @@ class Publisher(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'text/plain'
 
         try:
-            nickname = self.request.get('nickname')
+            nickname = unicode(self.request.get('nickname'))
             email = self.request.get('email')
             score = int(self.request.get('score'))
             turn = int(self.request.get('turns'))
             lat = float(self.request.get('lat'))
             lon = float(self.request.get('lon'))
-            city = self.request.get('city')
-            state = self.request.get('state')
-            country = self.request.get('country')
-            provider = self.request.get('provider')
+            city = unicode(self.request.get('city'))
+            state = unicode(self.request.get('state'))
+            country = unicode(self.request.get('country'))
+            provider = unicode(self.request.get('provider'))
             accuracy = float(self.request.get('accuracy'))
         except:
             self.response.headers['Content-Type'] = 'text/plain'
@@ -83,23 +91,30 @@ class Publisher(webapp2.RequestHandler):
             return None
 
         #if nickname and email and score and turn and lat and lon and provider and accuracy
-
-        player =  Player(email=email)
+        if not (nickname and email and score and turn and lat and lon and city and state
+                and country and provider and accuracy):
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.write('0')
+            return None
+        player_key = email+city+state+country
+        parent_key = user_key(email)
+        player = Player.get_by_key_name(player_key, parent=parent_key)
+        #The player entry do not exists (limited by location and email)
+        if not player:
+            player =  Player(parent=parent_key,key=player_key)
+            player.email = email
+            player.city = city
+            player.state = state
+            player.country = country
         player.nickname = nickname
         player.score = score
         player.turns = turn
         player.lat = lat
         player.lon = lon
-        player.city = city
-        player.state = state
-        player.country = country
         player.provider = provider
         player.accuracy = accuracy
         player.put()
-        self.response.write(player.key())
-        #else:
-        #    self.response.headers['Content-Type'] = 'text/plain'
-        #    self.response.write('0')
+        self.response.write(parent_key)
 
 
 app = webapp2.WSGIApplication([('/', MainPage), ('/publish', Publisher), ('/ranking', Response)], debug=True)
